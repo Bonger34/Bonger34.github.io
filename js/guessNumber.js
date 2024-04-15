9a4f588dd0fd24888dd225225e08f8e5283e366b
Bonger34 <2700875818@qq.com>
2024-04-15 22:43:17 +0800

    Update guessNumber

--- js/guessNumber.js
+++ js/guessNumber.js
@@ -1,18 +1,18 @@
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+var guessTimes = 0;
+
 function checkNumber() {
 	var isResultTrue = false;
-	if (!isResultTrue) {
-		var randomNumber = Math.floor(Math.random() * 10) + 1;
-	}
-	var guess;
-	guess = parseInt(document.getElementById('guessNumber').value);
-	if (isNaN(guess)) {
-		alert("请输入数字！")
-	} else if (guess < randomNumber) {
+	var guess = parseInt(document.getElementById('guessNumber').value);
+	guessTimes += 1;
+	if (guess < randomNumber) {
 		alert("小了，请再试一次！");
 	} else if (guess > randomNumber) {
 		alert("大了，请再试一次！");
-	} else {
-		alert("恭喜，猜对了！正确答案就是" + randomNumber + "。开始新游戏！");
+	} else if (guess = randomNumber) {
+		alert("恭喜，猜对了！正确答案就是" + randomNumber + "。\n你一共猜了" + guessTimes + "次。已开始新游戏！");
 		isResultTrue = true;
+		randomNumber = Math.floor(Math.random() * 10) + 1;
+		guessTimes = 0;
 	}
 }
