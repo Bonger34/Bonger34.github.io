d4e8b8b53aef4a9df789ab47632db74a7ce2ffab
Bonger34 <2700875818@qq.com>
2024-04-20 03:08:20 +0800

    Update index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-function openNav() {
-	document.getElementById("mySidenav").style.width = "200px";
-}
-
-function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
+function toggleNav() {
+	if (window.getComputedStyle(document.getElementsByClassName("sidenav")[0]).width === "0px") {
+		document.getElementsByClassName("sidenav")[0].style.width = "200px";
+	} else if (window.getComputedStyle(document.getElementsByClassName("sidenav")[0]).width === "200px"){
+		document.getElementsByClassName("sidenav")[0].style.width = "0px";
+	}
 }
