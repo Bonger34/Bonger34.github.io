0d4ee11ec75f7c52e5ba6e010092ff66eea89ed2
Bonger34 <115859117+Bonger34@users.noreply.github.com>
2024-04-28 23:22:47 +0800

    Increase the width of the sidenav Add favicon

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function toggleNav() {
 	if (window.getComputedStyle(document.getElementsByClassName("sidenav")[0]).width === "0px") {
-		document.getElementsByClassName("sidenav")[0].style.width = "200px";
-	} else if (window.getComputedStyle(document.getElementsByClassName("sidenav")[0]).width === "200px"){
+		document.getElementsByClassName("sidenav")[0].style.width = "400px";
+	} else if (window.getComputedStyle(document.getElementsByClassName("sidenav")[0]).width === "400px"){
 		document.getElementsByClassName("sidenav")[0].style.width = "0px";
 	}
 }
