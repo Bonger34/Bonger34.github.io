8ae7445e98ee2a5d83480f5040fbc988b70946c4
Bonger34 <115859117+Bonger34@users.noreply.github.com>
2024-05-05 05:33:58 +0800

    Clarified the alt attribute for hero in index.html Restricted the input number range for the guessing game Added a minimum guess count statistic for the guessing game Incorporated links to other number/letter guessing game projects

--- js/guessNumber.js
+++ js/guessNumber.js
@@ -1,18 +1,42 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
-var guessTimes = 0;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessTimes = 0;
+let minGuesses = Infinity;
+
+let guessInput = document.getElementById('guessNumber');
+
+guessInput.addEventListener('keyup', function(event) {
+	if (event.key === 'Enter') {
+		checkNumber();
+	}
+});
 
 function checkNumber() {
-	var isResultTrue = false;
-	var guess = parseInt(document.getElementById('guessNumber').value);
+	let guess = parseInt(document.getElementById('guessNumber').value);
 	guessTimes += 1;
-	if (guess < randomNumber) {
+	if (isNaN(guess) || guess < 1 || guess > 100) {
+		alert("请输入1到100之间的整数！");
+	} else if (guess < randomNumber) {
 		alert("小了，请再试一次！");
 	} else if (guess > randomNumber) {
 		alert("大了，请再试一次！");
-	} else if (guess = randomNumber) {
-		alert("恭喜，猜对了！正确答案就是" + randomNumber + "。\n你一共猜了" + guessTimes + "次。已开始新游戏！");
-		isResultTrue = true;
+	} else if (guess === randomNumber) {
+		alert("恭喜，猜对了！正确答案就是" + randomNumber + "。\n" +
+			"你一共猜了" + guessTimes + "次。已开始新游戏！");
+		if (guessTimes < minGuesses) {
+			minGuesses = guessTimes;
+			document.getElementById('minGuesses').innerHTML = minGuesses;
+		}
 		randomNumber = Math.floor(Math.random() * 100) + 1;
 		guessTimes = 0;
+	}
+	if (Infinity > minGuesses && minGuesses > 7) {
+		document.getElementsByClassName('minGuesses')[0].style.borderColor = "red";
+		document.getElementsByClassName('minGuesses')[0].style.backgroundColor = "rgba(255, 0, 0, 0.1)";
+	} else if (7 >= minGuesses && minGuesses > 4) {
+		document.getElementsByClassName('minGuesses')[0].style.borderColor = "yellow";
+		document.getElementsByClassName('minGuesses')[0].style.backgroundColor = "rgba(255, 255, 0, 0.1)";
+	} else if (4 >= minGuesses && minGuesses >= 1) {
+		document.getElementsByClassName('minGuesses')[0].style.borderColor = "green";
+		document.getElementsByClassName('minGuesses')[0].style.backgroundColor = "rgba(0, 255, 0, 0.1)";
 	}
 }
