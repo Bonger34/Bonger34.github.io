a5023efa34c3c21df4583ef17eea270f78b00671
Bonger <115859117+Bonger34@users.noreply.github.com>
2025-11-27 21:11:39 +0800

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,19 +1,13 @@
-import {GoogleGenerativeAI} from "@google/generative-ai";
-import {marked} from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import { GoogleGenAI } from "@google/genai";
 
-const API_KEY = "AIzaSyAYFmsGxIgBSF_LH30dfSa8Cdn7T0oszNs";
+const ai = new GoogleGenAI({ apiKey: "AIzaSyAYFmsGxIgBSF_LH30dfSa8Cdn7T0oszNs" });
 
-const genAI = new GoogleGenerativeAI(API_KEY);
-
-async function generate() {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({model: "gemini-pro"});
-    const prompt = document.getElementById("input").value;
-    const result = await model.generateContentStream(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-    document.getElementById("output").innerHTML = marked(text);
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
 }
 
-window.generate = generate;
+main();
